0d162e1b395f178db2d20b6e83e03ee291c0116e
Vikramjit Roy <vikramjitr@zeta.tech>
2024-03-07 20:22:45 +0530

    verify domain for facebook

---
+++ src/components/loadAnimation/LoadingAnimation.js
@@ -0,0 +1,13 @@
+// src/components/LoadingAnimation.js
+import React from 'react';
+import './loadAnimation.css';
+
+const LoadingAnimation = () => {
+  return (
+    <div className="loading-animation">
+      {/* Loading animation content goes here */}
+    </div>
+  );
+};
+
+export default LoadingAnimation;
